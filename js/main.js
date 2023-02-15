488253b7a8caf13a54b875ebc64745b70d39694a
Sadick707 <125457259+Sadick707@users.noreply.github.com>
2023-02-15 15:59:53 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let elMin = document.querySelector('.min');
+let elSec = document.querySelector('.sec');
+let elInp = document.querySelector('.inp');
+let elBtn= document.querySelector('.btn');
+let elLineInner = document.querySelector('.line_inner');
+
+elBtn.onclick = ()=>{
+  let min = 0;
+let sec = 0;
+let count = elInp.value;
